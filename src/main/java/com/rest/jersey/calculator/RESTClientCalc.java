2f2633ae2cc0024286eb98feca3dd5b31cd7de0a
luci649 <125581198+luci649@users.noreply.github.com>
2024-11-23 22:15:13 +0000

    added additional code to the client class to further test the cal function.

--- src/main/java/com/rest/jersey/calculator/RESTClientCalc.java
+++ src/main/java/com/rest/jersey/calculator/RESTClientCalc.java
@@ -10,16 +10,28 @@ import javax.ws.rs.client.Entity;
 import org.glassfish.jersey.client.ClientConfig;
 
 public class RESTClientCalc {
-	private static final String webServiceURI = "https://webapp-241122141130.azurewebsites.net";
+	private static final String webServiceURI = "http://localhost:8080/RESTCalculator";
 	public static void main(String[] args) {
 		ClientConfig clientConfig = new ClientConfig();
 		Client client = ClientBuilder.newClient(clientConfig);
 		URI serviceURI = UriBuilder.fromUri(webServiceURI).build();
 		WebTarget webTarget = client.target(serviceURI);
 		System.out.println("\nUsing GET QUERY parameters to invoke a RESTful service returning JSON output:");
-		System.out.println(webTarget.path("calc")
-			.queryParam("a", "74").queryParam("b", "12")
-			.queryParam("op", "+").request()
-			.accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "74").queryParam("b", "12").queryParam("op", "+")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "7").queryParam("b", "2").queryParam("op", "add")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "40").queryParam("b", "21").queryParam("op", "-")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "10").queryParam("b", "1").queryParam("op", "sub")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "20").queryParam("b", "2").queryParam("op", "*")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "11").queryParam("b", "15").queryParam("op", "mul")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "9").queryParam("b", "3").queryParam("op", "/")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
+		System.out.println(webTarget.path("calc").queryParam("a", "8").queryParam("b", "4").queryParam("op", "div")
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class));
 	}
 }
